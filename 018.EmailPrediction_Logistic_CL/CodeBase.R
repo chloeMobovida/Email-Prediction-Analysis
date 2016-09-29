363098c159e3f47eddf5f4b29d1e9bb9b340a747
Chloe Li <chloeli@doom-machine.wemobile>
2016-09-29 10:25:55 -0700

    logistic regression code base

--- 018.EmailPrediction_Logistic_CL/CodeBase.R
+++ 018.EmailPrediction_Logistic_CL/CodeBase.R
@@ -2,7 +2,7 @@
 #The purpose of this is to see goodness of fit for different formula combination
 
 
-#--------------------------------------------------------EDIT PART---------------------------------------------------------------#
+#--------------------------------------------------------PREPARATION---------------------------------------------------------------#
 
                 #clear environment if neccessary
                 rm(list = ls())
@@ -10,17 +10,25 @@
                 
                 #set your working directory
                 setwd("~/Documents/02. EmailPrediction_CL/018.EmailPrediction_Logistic_CL/")
-          
+                
+                source("./dtTransform_FUN.R") #trasnform data type #don't change
+                
                 #grep data insert data name
                 DT <- read.csv("./Data/eRFM_email_test1.csv")
                 #str(DT)
+                DT <- dtTransform(DT) #run this only once
+                
+ #--------------------------------------------------------EDIT PART---------------------------------------------------------------#
+                
                 
                 #setup independent variabe and dependent variable, create a formula, setup partition percentage
                 #make sure the name of column names match
                 percentPartition <- 0.75 #% of train set for partitioning
-                fml = opened ~ Ro + Rc + Fc + Fo
-                IV <- c("Ro", "Rc","Fc","Fo")
-                DV <- "opened" 
+                fml = opened ~ Ro + Rc + Fc + Fo + conversions + lifetime_sent + clusterNum 
+                IV <- c("Ro", "Rc","Fc","Fo","conversions", "lifetime_sent","clusterNum")
+                DV <- "opened"
+                TargetClassIdentify <- 0.5 #probability of hitting target (class of Good)
+                #might need to reset this threshold so that R can distinguish the class to run confusion matrix.
 
 #--------------------------------------------------------EDIT END---------------------------------------------------------------#
 #--------------------------------------------------------RUN AUTO---------------------------------------------------------------#
@@ -32,16 +40,14 @@
                 #install.packages("pacman")
                 pacman::p_load("party","dplyr", "caret","e1071","pscl","ROCR","ResourceSelection")
                 
-                source("./dtTransform_FUN.R") #trasnform data type
-                source("./logit_Model_FUN.R") #run logistic model
                 
-                DT <- dtTransform(DT)
+                source("./logit_Model_FUN.R") #run logistic model
 
 
                 #logit_model returns a full dataframe with prediction probability
-                DT <- logit_Model(DT, percentPartition, DV, IV, fml)
+                DT <- logit_Model(DT, percentPartition, DV, IV, fml,TargetClassIdentify)
                 
-                
+                #write.csv(DT, "Log_PredictionDT.csv")
                 
                 
                 
