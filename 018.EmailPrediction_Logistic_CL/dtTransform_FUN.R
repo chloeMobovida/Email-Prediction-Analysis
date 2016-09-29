46f16cf00f6e007ad86e65b7beb1071a57bce8ae
Chloe Li <chloeli@doom-machine.wemobile>
2016-09-29 10:26:42 -0700

    edit in data type transformation function

--- 018.EmailPrediction_Logistic_CL/dtTransform_FUN.R
+++ 018.EmailPrediction_Logistic_CL/dtTransform_FUN.R
@@ -4,11 +4,15 @@ dtTransform <- function(dataframe){
 	#reformat data
 	#change multiple variables into factor type
 	cols = c(2, 3, 4,5,8,9,10,11,12,13)
-	DT[cols] <- lapply(DT[cols], factor)
-	DT$riid <- as.character(DT$riid)
-
+	dataframe[cols] <- lapply(dataframe[cols], factor)
+	dataframe$riid <- as.character(dataframe$riid)
+  
+	numcol = c(6,7)
+	dataframe[numcol] <- lapply(dataframe[numcol], as.numeric)
 
 	#dont include sent == 0
-	DT <- filter(DT, sent != 0)
+	dataframe <- filter(dataframe, sent != 0)
+
+	return(dataframe)
 
 }
