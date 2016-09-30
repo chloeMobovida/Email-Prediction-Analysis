28d7c9634e47be923221e36cce14bb7f3e7f7bf4
Chloe Li <chloeli@doom-machine.wemobile>
2016-09-30 10:20:53 -0700

    Add IF function

--- 018.EmailPrediction_Logistic_CL/dtTransform_FUN.R
+++ 018.EmailPrediction_Logistic_CL/dtTransform_FUN.R
@@ -2,17 +2,26 @@
 
 dtTransform <- function(dataframe){
 	#reformat data
-	#change multiple variables into factor type
-	cols = c(2, 3, 4,5,8,9,10,11,12,13)
-	dataframe[cols] <- lapply(dataframe[cols], factor)
-	dataframe$riid <- as.character(dataframe$riid)
-  
-	numcol = c(6,7)
-	dataframe[numcol] <- lapply(dataframe[numcol], as.numeric)
+			dataframe[,"riid"] <- as.character(dataframe[,"riid"]) #make riid as character
 
-	#dont include sent == 0
-	dataframe <- filter(dataframe, sent != 0)
+			columnName <- c("sent","opened","clicked","converted","complained")
 
-	return(dataframe)
+			if(length(setdiff(columnName,colnames(dataframe))) == 0){
+				col=c("Ro","Rc","Fo","Fc","sent","opened","clicked","converted","complained") #select columns that need to be transformed to factor
+                dataframe[col] <- lapply(dataframe[col], factor)
+                #dont include sent == 0
+				dataframe <- filter(dataframe, sent != 0)
+				
+			}else{ col=c("Ro","Rc","Fo","Fc")
+					 dataframe[col] <- lapply(dataframe[col], factor)
+			}
+
+                #select columns need to transform to numeric
+                cols=c("lifetime_sent", "conversions")
+                dataframe[cols] <- lapply(dataframe[cols], as.numeric)
+
+				
+
+				return(dataframe)
 
 }
